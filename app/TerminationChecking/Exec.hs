10e359bbd2e824a5bc8d5e3c4afa6ad2d3ac8204
V. Jackson <vjjac@student.unimelb.edu.au>
2022-10-07 11:49:46 +1100

    term_check: make various small changes to Exec

--- app/TerminationChecking/Exec.hs
+++ app/TerminationChecking/Exec.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE TupleSections #-}
 
 module TerminationChecking.Exec where
 
+import Data.Bifunctor (bimap)
 import Data.List (find, permutations)
 import Control.Monad (join)
 import Data.Set (Set)
@@ -75,41 +77,34 @@ type Path = [PathToken]
 
 data SizeChange v = Var v | NatLit Integer | BoolLit Bool | Unit () deriving (Show, Eq, Ord)
 
-pattern_var_depths :: Eq v => Int -> Pattern v -> Path -> Path -> Bool -> [(SizeChange v, Double, Path, Path)]
-pattern_var_depths n (PVar x) s a b = [(Var x, (fromIntegral n) :: Double, reverse s, reverse a)]
-pattern_var_depths n PUnit s a b = [(Unit (), (fromIntegral n), reverse s, reverse a)]
-pattern_var_depths n (PPair p0 p1) s a b =
+pattern_var_depths :: Eq v => Int -> Path -> Path -> Bool -> Pattern v -> [(SizeChange v, Double, Path, Path)]
+pattern_var_depths n s a b (PVar x) = [(Var x, (fromIntegral n) :: Double, reverse s, reverse a)]
+pattern_var_depths n s a b PUnit = [(Unit (), (fromIntegral n), reverse s, reverse a)]
+pattern_var_depths n s a b (PPair p0 p1) =
   if b
-    then pattern_var_depths n p0 s (La:a) b ++ pattern_var_depths n p1 s (Ra:a) b
-    else pattern_var_depths n p0 s a b ++ pattern_var_depths n p1 s a b
-pattern_var_depths n (PNatLit num) s a b = [(NatLit num, (fromIntegral n), reverse s, reverse a)]
-pattern_var_depths n (PBoolLit bool) s a b = [(BoolLit bool, (fromIntegral n), reverse s, reverse a)]
-pattern_var_depths n (PSumL p) s a b = if b then pattern_var_depths n p (Ld:s) a b else pattern_var_depths n p s a b
-pattern_var_depths n (PSumR p) s a b = if b then pattern_var_depths n p (Rd:s) a b else pattern_var_depths n p s a b
-pattern_var_depths n (PRoll p) s a b = pattern_var_depths (n+1) p s a False
+    then pattern_var_depths n s (La:a) b p0 ++ pattern_var_depths n s (Ra:a) b p1
+    else pattern_var_depths n s a b p0 ++ pattern_var_depths n  s a b p1
+pattern_var_depths n s a b (PNatLit num) = [(NatLit num, (fromIntegral n), reverse s, reverse a)]
+pattern_var_depths n s a b (PBoolLit bool) = [(BoolLit bool, (fromIntegral n), reverse s, reverse a)]
+pattern_var_depths n s a b (PSumL p) = if b then pattern_var_depths n (Ld:s) a b p else pattern_var_depths n s a b p
+pattern_var_depths n s a b (PSumR p) = if b then pattern_var_depths n (Rd:s) a b p else pattern_var_depths n s a b p
+pattern_var_depths n s a b (PRoll p) = pattern_var_depths (n+1) s a False p
 
-
---term_var_depths :: Eq v => Int -> Term v -> Path -> [(v,Int,Path)]
---(fmap fromIntegral n) :: Maybe Double
-term_var_depths :: Maybe Int -> Term v -> Path -> Path -> Bool -> [(SizeChange v, Maybe Double, Path, Path)]
-term_var_depths n (TVar x) s a b = [(Var x, (fmap fromIntegral n), reverse s, reverse a)]
-term_var_depths n TUnit s a b = [(Unit (), (fmap fromIntegral n), reverse s, reverse a)]
-term_var_depths n (TPair p0 p1) s a b =
+term_var_depths :: Eq v => Maybe Int -> Path -> Path -> Bool -> Term v -> [(SizeChange v, Maybe Double, Path, Path)]
+term_var_depths n s a b (TVar x) = [(Var x, (fmap fromIntegral n), reverse s, reverse a)]
+term_var_depths n s a b TUnit = [(Unit (), (fmap fromIntegral n), reverse s, reverse a)]
+term_var_depths n s a b (TPair p0 p1) =
   if b
-    then term_var_depths n p0 s (La:a) b ++ term_var_depths n p1 s (Ra:a) b
-    else term_var_depths n p0 s a b ++ term_var_depths n p1 s a b
-term_var_depths n (TNatLit num) s a b = [(NatLit num, (fmap fromIntegral n), reverse s, reverse a)]
-term_var_depths n (TBoolLit bool) s a b = [(BoolLit bool, (fmap fromIntegral n), reverse s, reverse a)]
-term_var_depths n (TSumL p) s a b = if b then term_var_depths n p (Ld:s) a b else term_var_depths n p s a b
-term_var_depths n (TSumR p) s a b = if b then term_var_depths n p (Rd:s) a b else term_var_depths n p s a b
-term_var_depths n (TRoll p) s a b = term_var_depths ((+) <$> n <*> (Just 1)) p s a False
-term_var_depths n (TUnroll p) s a b = term_var_depths ((-) <$> n <*> (Just 1)) p s a b
-term_var_depths n (TApp p r) s a b = term_var_depths n p s a False ++ term_var_depths Nothing r s a False-- THIS NEEDS FIXING
--- ADD IN AN IF
-
-app_primary_term :: Term v -> Term v
-app_primary_term (TApp s _) = app_primary_term s
-app_primary_term s = s
+    then term_var_depths n s (La:a) b p0 ++ term_var_depths n s (Ra:a) b p1
+    else term_var_depths n s a b p0 ++ term_var_depths n s a b p1
+term_var_depths n s a b (TNatLit num) = [(NatLit num, (fmap fromIntegral n), reverse s, reverse a)]
+term_var_depths n s a b (TBoolLit bool) = [(BoolLit bool, (fmap fromIntegral n), reverse s, reverse a)]
+term_var_depths n s a b (TSumL p) = if b then term_var_depths n (Ld:s) a b p else term_var_depths n s a b p
+term_var_depths n s a b (TSumR p) = if b then term_var_depths n (Rd:s) a b p else term_var_depths n s a b p
+term_var_depths n s a b (TRoll p) = term_var_depths ((+) <$> n <*> (Just 1)) s a False p
+term_var_depths n s a b (TUnroll p) = term_var_depths ((-) <$> n <*> (Just 1)) s a b p
+term_var_depths n s a b (TApp p r) = term_var_depths n s a False p ++ term_var_depths Nothing s a False r
+-- TODO: add if-then-else?
 
 term_find_rec :: Eq v => v -> Term v -> [Term v]
 term_find_rec rn (TPair s0 s1) = term_find_rec rn s0 ++ term_find_rec rn s1
@@ -187,21 +182,17 @@ matrixify name fun = matrixified
               , join (map (\x -> term_var_depths 0 x [] [] True) (term_find_rec name s))
               )
             ) fun-}
-        m = join $ map
-                (\(p, s) -> go $
-                  ( map (\(a, b, c, d) -> (a, Just b, c, d)) (pattern_var_depths 0 p [] [] True)
-                  , map (\x -> term_var_depths (Just 0) x [] [] True) (term_find_rec name s)
-                  )
-                ) fun where
-                    go (x, []) = []
-                    go (x, (y:ys)) = (x, y) : (go (x, ys))
+        m = do
+              (p, s) <- fun
+              let a = map (\(a, b, c, d) -> (a, Just b, c, d)) (pattern_var_depths 0 [] [] True p)
+              map ((a,) . term_var_depths (Just 0) [] [] True) (term_find_rec name s)
+
         -- [] in the term_depth corresponds to having a constant in there
         -- so we filter out by it so as to not consider things like f(x) = f(3) etc.
         -- Currently not working quite as intended as function calls are assigned [] depth
         -- as a placeholder, meaning we assume all auxilliary functions terminate (the intention
         -- was to make the opposite assumption for now)
-        filtM = filter (\x -> snd x /= []) m
-
+        filtM = filter (not.null.snd) m
 
         {-
         m' is a further filtration of filtM which removes non-max depth variables and collectes
@@ -214,9 +205,7 @@ matrixify name fun = matrixified
         [({n, t1, t2}, t1), ({n', t1', t2'}, t2), ({n, t1, t2}, {n', t1, t1'}), ({n', t1', t2'}, t2')]
 
         -}
-        isVar (Var x) = True
-        isVar _ = False
-        m' = map (\(xs, ys) -> (list_convert xs, list_convert ys)) filtM
+        m' = map (bimap list_convert list_convert) filtM
 
         {-
         This turns our pairs of depths and arguments etc. into something resembling our final matrix; a matrix of
@@ -244,7 +233,7 @@ matrixify name fun = matrixified
             the entries by their argument by placing them in a unique column associated with their
             argument (of course, the row is already taken care of)
         -}
-        matrixified = map (\a -> sortByArg a matWithDisjArgs) arguments
+        matrixified = map (flip sortByArg matWithDisjArgs) arguments
 
 
 
@@ -263,7 +252,7 @@ mkUniq = rdHelper []
     where
         rdHelper seen [] = seen
         rdHelper seen (x:xs) | x `elem` seen = rdHelper seen xs
-                                | otherwise = rdHelper (seen ++ [x]) xs
+                             | otherwise = rdHelper (seen ++ [x]) xs
 
 -- Checks if any arguments appear on the rhs that aren't on the lhs and puts a ? in that matrix entry
 gain_check xs ys = [P (Right Na) from [] arg | (v, n, from, arg) <- ys, (filter (\(_, _, _, a) -> a == arg) xs) == []]
@@ -298,8 +287,9 @@ toEntry (P (Right v) f t p) = Sym v
 
 -- Extracts all of the pre-matrix entries in a particular argument for all the recursive calls.
 sortByArg a [] = []
-sortByArg a (x:xs) = let l = [y | y <- x, argOf y == a]
-                     in if l == [] then (Num 0):(sortByArg a xs) else (map toEntry l) ++ sortByArg a xs
+sortByArg a (x:xs) =
+  let l = [y | y <- x, argOf y == a]
+  in if l == [] then (Num 0):(sortByArg a xs) else (map toEntry l) ++ sortByArg a xs
 
 
 makeDisjArgsAllDisjs ds call = call ++ (map (makeDisjArgs call) ds)
@@ -343,25 +333,19 @@ pair_list (a, (b:bs)) = (a, b) : (pair_list (a, bs))
 setify (v, n, disj, arg) = ((Set.singleton v), n, disj, arg)
 list_convert xs = mkUniq $ map (\y -> process_depths (setify y) xs) xs --mkUniq $ (process_depths (setify x) xs):(list_convert xs)
 
+Nothing >>> a = True
+a >>> Nothing = False
+a >>> b = a > b
+
 -- Gets max depth variables and puts them all in a set so we only have 1 entry per arg
 process_depths ret [] = ret
 process_depths (vs, n, disj, arg) ((v', n', disj', arg'):xs)    | (arg == arg') && (n' >>> n)  = process_depths ((Set.singleton v'), n', disj', arg') xs
                                                                 | (arg == arg') && (n' == n)   = process_depths ((Set.insert v' vs), n, disj, arg) xs
                                                                 | otherwise                    = process_depths (vs, n, disj, arg) xs
 
-
-
-
-Nothing >>> a = True
-a >>> Nothing = False
-a >>> b = a > b
-
-mat' name fun = join $ map
-                (\(p, s) -> go $
-                  ( map (\(a, b, c, d) -> (a, Just b, c, d)) (pattern_var_depths 0 p [] [] True)
-                  , map (\x -> term_var_depths (Just 0) x [] [] True) (term_find_rec name s)
-                  )
-                ) fun where
-                    go (x, []) = []
-                    go (x, (y:ys)) = (x, y) : (go (x, ys))
+mat' name fun =
+  do
+    (p,s) <- fun
+    let a = map (\(a, b, c, d) -> (a, Just b, c, d)) (pattern_var_depths 0 [] [] True p)
+    map ((a,) . (term_var_depths (Just 0) [] [] True)) (term_find_rec name s)
 
